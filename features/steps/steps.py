e5bc5e4e81a464f7a305c73bbb8a342dcbd9fffc
smiroshnikov <sergeim.qa@gmail.com>
2017-08-21 22:56:03 +0300

    Behavior driven development, just a proff of concept . Managed to start from JIdea on fucking Windows

---
+++ features/steps/steps.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from behave import *
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+@given('website "{url}"')
+def step(context, url):
+    driver = webdriver.Chrome("C:\Webdrivers\chromedriver.exe")
+    context.browser = driver
+    context.browser.maximize_window()
+    context.browser.get("http://ya.ru")
+
+
+@then("push button with text '{text}'")
+def step(context, text):
+    WebDriverWait(context.browser, 120).until(
+        EC.element_to_be_clickable((By.XPATH, '//button'))
+    )
+    context.browser.find_element_by_xpath('//button').click()
+
+
+@then("page include text '{text}'")
+def step(context, text):
+    WebDriverWait(context.browser, 120).until(
+        EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "%s")]' % text))
+    )
+    assert context.browser.find_element_by_xpath('//*[contains(text(), "%s")]' % text)
+    context.browser.quit()
